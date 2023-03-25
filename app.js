6dbba5381866f42b7f378027fd4de44fdb305bbe
wihctoh <rivertoxi911@gmail.com>
2023-03-25 23:57:19 +0300

    oop

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-const dynamicTime = document.querySelector(".time");
-const dynamicDate = document.querySelector(".date");
+// const dynamicTime = document.querySelector(".time");
+// const dynamicDate = document.querySelector(".date");
 
-function clock(dynamicTime_) {
-  let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-  let clock1 = `${hours}:${minutes}:${seconds}`;
+// function clock(dynamicTime_) {
+//   let now = new Date();
+//   let hours = now.getHours();
+//   let minutes = now.getMinutes();
+//   let seconds = now.getSeconds();
+//   let clock1 = `${hours}:${minutes}:${seconds}`;
 
-  dynamicTime_.innerHTML = clock1;
+//   dynamicTime_.innerHTML = clock1;
+// }
+
+// function date(dynamicDate_) {
+//   let now = new Date().toLocaleDateString();
+
+//   dynamicDate_.innerHTML = now;
+// }
+
+// setInterval(date, 60000, dynamicDate);
+// setInterval(clock, 1000, dynamicTime);
+
+class AppleDisplay {
+  dynamicTime = document.querySelector(".time");
+  dynamicDate = document.querySelector(".date");
+
+  clock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let clock1 = `${hours}:${minutes}:${seconds}`;
+
+    this.dynamicTime.innerHTML = clock1;
+  }
+
+  date() {
+    let now = new Date().toLocaleDateString();
+
+    this.dynamicDate.innerHTML = now;
+  }
 }
 
-function date(dynamicDate_) {
-  let now = new Date().toLocaleDateString();
+const appleDisplay = new AppleDisplay();
 
-  dynamicDate_.innerHTML = now;
-}
+// appleDisplay.date();
+// appleDisplay.clock();
 
-setInterval(date, 60000, dynamicDate);
-setInterval(clock, 1000, dynamicTime);
+setInterval(appleDisplay.date(), 60000, appleDisplay.dynamicDate);
+setInterval(appleDisplay.clock(), 1000, appleDisplay.dynamicTime);
