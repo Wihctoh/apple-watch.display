962d28d69f846877aecfb37ee9531db117f4c078
wihctoh <rivertoxi911@gmail.com>
2023-03-26 15:41:36 +0300

    oop fix

--- app.js
+++ app.js
@@ -21,30 +21,30 @@
 // setInterval(clock, 1000, dynamicTime);
 
 class AppleDisplay {
-  dynamicTime = document.querySelector(".time");
-  dynamicDate = document.querySelector(".date");
+  constructor() {
+    setInterval(this.date, 1000, this.dynamicDate);
+    setInterval(this.clock, 1000, this.dynamicTime);
+  }
 
   clock() {
+    const dynamicTime = document.querySelector(".time");
+
     let now = new Date();
     let hours = now.getHours();
-    let minutes = now.getMinutes();
+    let minutes = now.getMinutes(); 
     let seconds = now.getSeconds();
     let clock1 = `${hours}:${minutes}:${seconds}`;
 
-    this.dynamicTime.innerHTML = clock1;
+    dynamicTime.innerHTML = clock1;
   }
 
   date() {
+    const dynamicDate = document.querySelector(".date");
+
     let now = new Date().toLocaleDateString();
 
-    this.dynamicDate.innerHTML = now;
+    dynamicDate.innerHTML = now;
   }
 }
 
 const appleDisplay = new AppleDisplay();
-
-// appleDisplay.date();
-// appleDisplay.clock();
-
-setInterval(appleDisplay.date(), 60000, appleDisplay.dynamicDate);
-setInterval(appleDisplay.clock(), 1000, appleDisplay.dynamicTime);
