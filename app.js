c00d9c7eddb48a408d733a8c77374b1983644794
wihctoh <rivertoxi911@gmail.com>
2023-03-27 20:21:44 +0300

    fix

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 // const dynamicTime = document.querySelector(".time");
 // const dynamicDate = document.querySelector(".date");
 
-// function clock(dynamicTime_) {
+// function clock() {
 //   let now = new Date();
-//   let hours = now.getHours();
-//   let minutes = now.getMinutes();
-//   let seconds = now.getSeconds();
-//   let clock1 = `${hours}:${minutes}:${seconds}`;
 
-//   dynamicTime_.innerHTML = clock1;
+//   dynamicTime.innerHTML = now.toLocaleTimeString("BY");
+//   dynamicDate.innerHTML = now.toLocaleDateString("BY");
 // }
 
-// function date(dynamicDate_) {
-//   let now = new Date().toLocaleDateString();
-
-//   dynamicDate_.innerHTML = now;
-// }
-
-// setInterval(date, 60000, dynamicDate);
-// setInterval(clock, 1000, dynamicTime);
+// setInterval(clock, 1000);
 
 class AppleDisplay {
   constructor() {
